930ae641d4af75fa5d42b10a6abe4e7f8084bf37
mafh <1193482868@qq.com>
2017-10-18 17:11:32 +0800

    first commit

---
+++ src/main/java/xin/mafh/jms/queue/AppComsumer.java
@@ -0,0 +1,39 @@
+package xin.mafh.jms.queue;
+
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class AppComsumer {
+    public static final String url = "tcp://www.mafh.xin:61616";
+    public static final String queueName = "queue-test";
+    public static void main(String[] args) throws JMSException {
+//        1.创建connectionFactory
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(url);
+//        2.创建connection
+        Connection connection = activeMQConnectionFactory.createConnection();
+//        3.启动连接
+        connection.start();
+//        4.创建会话
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//        5.创建一个目标
+        Destination destination = session.createQueue(queueName);
+//        6.创建一个消费者
+        MessageConsumer messageConsumer = session.createConsumer(destination);
+//        7.创建一个监听器
+        messageConsumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage)message;
+//                8.接受消息
+                try {
+                    System.out.println("接收消息："+textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+//        9.关闭连接
+//        connection.close();
+    }
+}
---
+++ src/main/java/xin/mafh/jms/queue/AppProducer.java
@@ -0,0 +1,37 @@
+package xin.mafh.jms.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ *
+ */
+public class AppProducer {
+    public static final String url = "tcp://www.mafh.xin:61616";
+    public static final String queueName = "queue-test";
+    public static void main(String[] args) throws JMSException {
+//        1.创建connectionFactory
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(url);
+//        2.创建connection
+        Connection connection = activeMQConnectionFactory.createConnection();
+//        3.启动连接
+        connection.start();
+//        4.创建会话
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//        5.创建一个目标
+        Destination destination = session.createQueue(queueName);
+//        6.创建一个生产者
+        MessageProducer messageProducer = session.createProducer(destination);
+
+        for (int i = 0; i < 100; i++) {
+//            7.创建消息
+            TextMessage textMessage = session.createTextMessage("text"+i);
+//            8.发布消息
+            messageProducer.send(textMessage);
+            System.out.println("生产者发送消息："+textMessage.getText());
+        }
+//        9.关闭连接
+        connection.close();
+    }
+}
